4056bc772142bd812e166d5a155bcb607c0acef1
Gourav Kumar <125178399+Gouravkumar717@users.noreply.github.com>
2024-09-11 20:39:34 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "test-server" {
   ami                    = "ami-0e86e20dae9224db8"
   instance_type          = "t2.micro"
   key_name               = "myedkey"
-  vpc_security_group_ids = ["sg-0d38fda416689c02b"]
+  #vpc_security_group_ids = ["sg-0d38fda416689c02b"]
   #subnet_id              = "0878d772e5548846a"  # Specify the correct subnet here
 
   connection {
